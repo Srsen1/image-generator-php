eeeee5bb4c27387b39229e3a97ef90011f5de333
Otto Srsen <otacmelak@gmail.com>
2023-05-23 07:36:31 +0000

    modified:   www/index.php

--- www/index.php
+++ www/index.php
@@ -1,22 +1,40 @@
 <?php
 
-$header = "";
-$text1 = "";
-$text2 = "";
+$header = "Hlavni Nadpis";
+$text = array("Ukazka");
+$number_of_text = count($text);
 
-$img1 = "";
-$img2 = "";
+$img = array('placeholder.png');
+$number_of_img = count($img);
 
-// Create a blank image and add some text
-$im = imagecreatetruecolor(255,20);
-$text_color = imagecolorallocate($im, 233, 14, 91);
-imagestring($im, 1, 5, 5,  'A Simple Text String', $text_color);
+if ($number_of_img != $number_of_text){
+    echo "Error";
+}
 
-// Set the content type header - in this case image/jpeg
+$main_size = array(300, 300);
+
+$main_canvas = imagecreatetruecolor($main_size[0], $main_size[1]);
+
+$text_color = imagecolorallocate($main_canvas, 255, 255, 255);
+$header_color = imagecolorallocate($main_canvas, 200, 200, 200);
+
+imagestring($main_canvas, 4, 8, 40,  $text[0], $text_color);
+imagestring($main_canvas, 10, 80, 10,  $header, $header_color);
+
+$dest = imagecreatetruecolor(300, 300);
+$pre_dest = imagecreatetruecolor(100, 100);
+
+$i = 0;
+foreach($img as $img_for_resize){
+    $size_img = getimagesize($img_for_resize);
+    $src = imagecreatefrompng($img_for_resize);
+    imagecopyresized($pre_dest, $src, 0, 0, 0, 0, $main_size[0]*0.25, $main_size[1]*0.25, $size_img[0], $size_img[1]);
+    imagecopymerge($main_canvas, $pre_dest, $i*$main_size[0]*0.25+$i*5+10, 60, 0, 0, 100, 100, 100);
+    $i++;
+}
+
+
 header('Content-Type: image/jpeg');
+imagejpeg($main_canvas);
 
-// Output the image
-imagejpeg($im);
-
-// Free up memory
-imagedestroy($im);
+imagedestroy($dest);
