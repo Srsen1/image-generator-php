939d93232a5727d3e40aec6c4e4729da8ad2766e
Otto Srsen <otacmelak@gmail.com>
2023-05-24 09:02:47 +0000

    modified:   index.php

--- index.php
+++ index.php
@@ -23,22 +23,21 @@ function text_to_image($text, $image_width, $colour = array(0,244,34), $backgrou
 }
 # nastaveni
 $header = "Hlavni Napis";
-$text = array('Krok 1 je: umyj si ruce', "Krok 2: tady j", "Ukazka", 'Krok 1 je: umyj si ruce', "Ukazka", 'Krok 1 je: umyj si ruce', "Ukazka", 'Krok 1 je: umyj si ruce');
+$text = array('Krok 1 je: umyj si ruce Krok 1 je: umyj si ruce', "Krok 2: tady j", "Ukazka", 'Krok 1 je: umyj si ruce', "Ukazka", 'Krok 1 je: umyj si ruce', "Ukazka", 'Krok 1 je: umyj si ruce');
 $img = array("placeholder.png", "placeholder.png", "placeholder.png", "placeholder.png", "placeholder.png", "placeholder.png", "placeholder.png", "placeholder.png");
 $img_count = count($img);
 
-$main_size = array(500, 700);
+$main_size = array(600, 850);
 $background = array(0, 0, 0);
 $text_color = array(255, 255, 255);
 $header_color_ar = array(200, 200, 200);
 $top_margin = ($img_y/1.5);
 $side_margin = $main_size[0]/16;
-$img_margin_top_ratio = 1.5; #pomer mezi velikosti obrazku a vrchni mezerou
+$img_margin_top_ratio = 2.5; #pomer mezi velikosti obrazku a vrchni mezerou
 # konec nastaveni
 
 $main_canvas = imagecreatetruecolor($main_size[0], $main_size[1]);
 $header_color = imagecolorallocate($main_canvas, $header_color_ar[0], $header_color_ar[1], $header_color_ar[2]);
-$main_canvas = imagecreatetruecolor($main_size[0], $main_size[1]);
 imagefill($main_canvas, 0, 0, imagecolorallocate($main_canvas, $background[0], $background[1], $background[2]));
 
 imagestring($main_canvas, 10, ($main_size[0]-strlen($header)*10)/2, 20,  $header, $header_color);
@@ -68,13 +67,23 @@ foreach($img as $i => $img_for_resize){
     }
     $top_margin = ($img_y/$img_margin_top_ratio);
     $x_position = (($main_size[0] - (($main_size[0]*0.25)*$this_row+$side_margin*($this_row-1))) /2) + (($i%3)*$main_size[0]*0.25+($i%3)*$side_margin);
-    $y_position = $top_margin*(($i-$i%3)/3)+($img_y*(($i-$i%3)/3)+$img_y/1.3)+$row_height[($i-$i%3)/3];
-    $y_position_text = $top_margin*(($i-$i%3)/3)+$img_y*(($i-$i%3)/3)+$img_y/1.3-10;
+    $row_h = 0;
+    $row_h_text = 0;
+    foreach($row_height as $r_i => $row){
+        if($r_i < (($i-$i%3)/3)){
+            $row_h_text += $row;
+        }
+        if($r_i == (($i-$i%3)/3)){
+            $row_h = $row_h_text+$row;
+        }
+    }
+    $y_position = $top_margin*(($i-$i%3)/3)+($img_y*(($i-$i%3)/3)+$img_y/1.3)+$row_h;
+    $y_position_text = $top_margin*(($i-$i%3)/3)+$img_y*(($i-$i%3)/3)+$img_y/1.3-10+$row_h_text;
 
     imagecopyresized($pre_dest, $src, 0, 0, 0, 0, $main_size[0]*0.25, ($main_size[0]*0.25)*$ratio, $size_img[0], $size_img[1]);
     if(strlen($text[$i]) > $main_size[0]*0.25/10){
         $text_in_image = text_to_image($text[$i], $main_size[0]*0.25, $text_color, $background);
-        imagecopymerge($main_canvas, $text_in_image[0], $x_position, $y_position_text, 0, 0, 100, $text_in_image[1], 100);
+        imagecopymerge($main_canvas, $text_in_image[0], $x_position, $y_position_text, 0, 0, $main_size[0]*0.25, $text_in_image[1], 100);
     }else{
         imagestring($main_canvas, 4,  $x_position,  $y_position_text, $text[$i], imagecolorallocate($main_canvas, $text_color[0], $text_color[1], $text_color[2]));
     }
