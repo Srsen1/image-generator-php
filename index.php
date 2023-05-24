020542f0129115bfabd0e8e9f2d180873484d9f2
Otto Srsen <otacmelak@gmail.com>
2023-05-24 08:36:42 +0000

    modified:   index.php 	deleted:    www/index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ function text_to_image($text, $image_width, $colour = array(0,244,34), $backgrou
 {
     $font = 4;
     $line_height = 15;
-    $padding = 0;
+    $padding = 2;
     $text = wordwrap($text, ($image_width/10));
     $lines = explode("\n", $text);
     $height = (count($lines) * $line_height) + ($padding * 2);
@@ -23,14 +23,17 @@ function text_to_image($text, $image_width, $colour = array(0,244,34), $backgrou
 }
 # nastaveni
 $header = "Hlavni Napis";
-$text = array('Krok 1 je: umyj si ruce', "Krok 2: tady", "Ukazka", 'Krok 1 je: umyj si ruce');
-$img = array("placeholder.png", "placeholder.png", "placeholder.png", "placeholder.png");
+$text = array('Krok 1 je: umyj si ruce', "Krok 2: tady j", "Ukazka", 'Krok 1 je: umyj si ruce', "Ukazka", 'Krok 1 je: umyj si ruce', "Ukazka", 'Krok 1 je: umyj si ruce');
+$img = array("placeholder.png", "placeholder.png", "placeholder.png", "placeholder.png", "placeholder.png", "placeholder.png", "placeholder.png", "placeholder.png");
 $img_count = count($img);
 
-$main_size = array(400, 500);
+$main_size = array(500, 700);
 $background = array(0, 0, 0);
 $text_color = array(255, 255, 255);
 $header_color_ar = array(200, 200, 200);
+$top_margin = ($img_y/1.5);
+$side_margin = $main_size[0]/16;
+$img_margin_top_ratio = 1.5; #pomer mezi velikosti obrazku a vrchni mezerou
 # konec nastaveni
 
 $main_canvas = imagecreatetruecolor($main_size[0], $main_size[1]);
@@ -49,29 +52,33 @@ foreach($text as $i_text => $v_text){
 }
 
 foreach($img as $i => $img_for_resize){
-    $pre_dest = imagecreatetruecolor(100, 100);
     $size_img = getimagesize($img_for_resize);
+    $ratio = $size_img[0]/$size_img[1];
+    $img_y = (($main_size[0]*0.25)*$ratio);
+    $pre_dest = imagecreatetruecolor($main_size[0]*0.25, $img_y);
     $src = imagecreatefrompng($img_for_resize);
-    $top_margin = $main_size[1]/6;
     if($img_count-$i > 2 or $img_count%3 == 0){
         $this_row = 3;
     }elseif($img_count%3 == 1 and $img_count-$i == 2){
         $this_row = 3;
     }elseif($img_count%3 == 2){
-        $this_row = 1;
+        $this_row = 2;
     }else{
-        $this_row = -1;
+        $this_row = 1;
     }
-    $margin = 10;
-    imagecopyresized($pre_dest, $src, 0, 0, 0, 0, $main_size[0]*0.25, $main_size[1]*0.25, $size_img[0], $size_img[1]);
-    $x_position = (($main_size[1] - (100+$margin)*$this_row) /4) + (($i%3)*$main_size[0]*0.25+($i%3)*$margin);
+    $top_margin = ($img_y/$img_margin_top_ratio);
+    $x_position = (($main_size[0] - (($main_size[0]*0.25)*$this_row+$side_margin*($this_row-1))) /2) + (($i%3)*$main_size[0]*0.25+($i%3)*$side_margin);
+    $y_position = $top_margin*(($i-$i%3)/3)+($img_y*(($i-$i%3)/3)+$img_y/1.3)+$row_height[($i-$i%3)/3];
+    $y_position_text = $top_margin*(($i-$i%3)/3)+$img_y*(($i-$i%3)/3)+$img_y/1.3-10;
+
+    imagecopyresized($pre_dest, $src, 0, 0, 0, 0, $main_size[0]*0.25, ($main_size[0]*0.25)*$ratio, $size_img[0], $size_img[1]);
     if(strlen($text[$i]) > $main_size[0]*0.25/10){
         $text_in_image = text_to_image($text[$i], $main_size[0]*0.25, $text_color, $background);
-        imagecopymerge($main_canvas, $text_in_image[0], $x_position, $top_margin*($i-$i%3)+$top_margin, 0, 0, 100, $text_in_image[1], 100);
+        imagecopymerge($main_canvas, $text_in_image[0], $x_position, $y_position_text, 0, 0, 100, $text_in_image[1], 100);
     }else{
-        imagestring($main_canvas, 4,  $x_position,  $top_margin*($i-$i%3)+$top_margin, $text[$i], imagecolorallocate($main_canvas, $text_color[0], $text_color[1], $text_color[2]));
+        imagestring($main_canvas, 4,  $x_position,  $y_position_text, $text[$i], imagecolorallocate($main_canvas, $text_color[0], $text_color[1], $text_color[2]));
     }
-    imagecopymerge($main_canvas, $pre_dest, $x_position, ($top_margin*($i-$i%3)+$main_size[1]/5)+$row_height[($i-$i%3)/3], 0, 0, 100, 100, 100);
+    imagecopymerge($main_canvas, $pre_dest, $x_position, $y_position, 0, 0, $main_size[0]*0.25, ($main_size[0]*0.25)*$ratio, 100);
 }
 
 header('Content-Type: image/jpeg');
